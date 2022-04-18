12bb96042bad787d93a5d05e83851413b6f28395
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-18 17:38:48 +0500

    Fix linters

--- script.js
+++ script.js
@@ -3,41 +3,44 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const collection = document.querySelector('.collection');
 
-let books = [];
-function bookObject(title, author, id) {
-    this.bookId = id;
-    this.bookTitle = title;
-    this.bookAuthor = author;
+const books = [];
+
+function BookObject(title, author, id) {
+  this.bookTitle = title;
+  this.bookAuthor = author;
+  this.bookId = id;
 }
 let i = 0;
 
-addBtn.addEventListener("click", function () {
-    if (title.value == "" || author.value == "") {
-        alert("Empty Input!")
-    } else {
-        let book = new bookObject(title.value, author.value, i);
-        books.push(book);
-        localStorage.setItem(0, JSON.stringify(books));
-        displayBooks();
-        clearInputs();
-        i += 1;
-    }
-})
+function clearInputs() {
+  title.value = '';
+  author.value = '';
+}
 
 function displayBooks() {
-    let displayTitle = document.createElement('p');
-    let displayAuthor = document.createElement('p');
-    let removeBtn = document.createElement('button');
-    removeBtn.addEventListener('click', removeBook(i));
-    let line = document.createElement('hr');
-    removeBtn.textContent = "Remove";
-    
-    displayTitle.textContent = books[i].bookTitle;
-    displayAuthor.textContent = books[i].bookAuthor;
-    collection.append(displayTitle, displayAuthor, removeBtn, line);
+  const displayTitle = document.createElement('p');
+  const displayAuthor = document.createElement('p');
+  const line = document.createElement('hr');
+  const removeBtn = document.createElement('button');
+  removeBtn.setAttribute('id', i);
+  removeBtn.textContent = 'Remove';
+  removeBtn.addEventListener('click', () => {
+  });
+
+  displayTitle.textContent = books[i].bookTitle;
+  displayAuthor.textContent = books[i].bookAuthor;
+  collection.append(displayTitle, displayAuthor, removeBtn, line);
 }
 
-function clearInputs() {
-    title.value = '';
-    author.value = '';
-}
+addBtn.addEventListener('click', () => {
+  if (title.value === '' || author.value === '') {
+    alert('Empty Input!');
+  } else {
+    const book = new BookObject(title.value, author.value, i);
+    books.push(book);
+    localStorage.setItem(0, JSON.stringify(books));
+    displayBooks();
+    clearInputs();
+    i += 1;
+  }
+});
