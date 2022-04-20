e571f7bfa9fba4c826f2b12f0a20c4ce77811660
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-04-20 12:44:25 +0500

    Add active states on link buttons

--- script.js
+++ script.js
@@ -118,18 +118,27 @@ const addNewSec = document.querySelector('.addNew');
 const contactSec = document.querySelector('.contact');
 
 listBtn.addEventListener('click', () => {
+  listBtn.classList.add('active');
+  addNewBtn.classList.remove('active');
+  contactBtn.classList.remove('active');
   listSec.style.display = 'block';
   addNewSec.style.display = 'none';
   contactSec.style.display = 'none';
 });
 
 addNewBtn.addEventListener('click', () => {
+  listBtn.classList.remove('active');
+  addNewBtn.classList.add('active');
+  contactBtn.classList.remove('active');
   listSec.style.display = 'none';
   addNewSec.style.display = 'block';
   contactSec.style.display = 'none';
 });
 
 contactBtn.addEventListener('click', () => {
+  listBtn.classList.remove('active');
+  addNewBtn.classList.remove('active');
+  contactBtn.classList.add('active');
   listSec.style.display = 'none';
   addNewSec.style.display = 'none';
   contactSec.style.display = 'flex';
